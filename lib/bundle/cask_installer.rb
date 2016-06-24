f5308346550b10b52939d5dfac422844ef698d73
Markus Doits <markus.doits@stellenticket.de>
2016-06-24 10:37:44 +0200

    force install updated cask so it overwrites existing app

--- lib/bundle/cask_installer.rb
+++ lib/bundle/cask_installer.rb
@@ -1,18 +1,20 @@
 module Bundle
   class CaskInstaller
     def self.install(name, options = {})
+      args = options.fetch(:args, []).map { |k, v| "--#{k}=#{v}" }
+
       if installed_casks.include? name
         if cask_up_to_date?(name)
           puts "Skipping install of #{name} cask. It is already installed." if ARGV.verbose?
           return true
         else
+          # without `--force` it will not overwrite the already existing .app
+          args.push '--force'
           puts "Cask #{name} is outdated." if ARGV.verbose?
         end
       elsif ARGV.verbose?
         puts "Cask #{name} is not installed."
       end
-
-      args = options.fetch(:args, []).map { |k, v| "--#{k}=#{v}" }
 
       puts "Installing #{name} cask." if ARGV.verbose?
       if (success = Bundle.system "brew", "cask", "install", name, *args)
