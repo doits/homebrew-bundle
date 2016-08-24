65907879ab715ee6bb1e072b9bab8b76fe045e8e
Markus Doits <markus.doits@stellenticket.de>
2016-08-24 13:17:27 +0200

    fix checking of outdated casks due to info output changes

--- lib/bundle/cask_installer.rb
+++ lib/bundle/cask_installer.rb
@@ -29,12 +29,33 @@ module Bundle
     end
 
     def self.cask_up_to_date?(name)
-      # `brew cask info` outputs `Not installed` when a cask's version has been
-      # updated and old one was installed before. It does this for never
-      # installed casks, too. So this method cannot differentiate between an
-      # outdated cask and a not installed cask. When the CLI output of `brew
-      # cask` changes, this has to be updated, too.
-      `brew cask info #{name}`.index('Not installed').nil?
+      # Determine it by looking at info outpu
+      info_output = `brew cask info #{name}`
+
+      # `brew cask info` outputs `Not installed` when a cask's is not
+      # installed. So this is not up to date.
+      return false if info_output.index('Not installed')
+
+      # For an installed casks, it outputs the installed version number after
+      # the cask name. For example:
+      #
+      # firefox: 48.0.1
+      # https://www.mozilla.org/en-US/firefox/
+      # /usr/local/Caskroom/firefox/47.0 (2 files, 589.4K)
+      # /usr/local/Caskroom/firefox/48.0 (68B)
+      # From: https://github.com/caskroom/homebrew-cask/blob/master/Casks/firefox.rb
+      # ==> Name
+      # Mozilla Firefox
+      # ==> Artifacts
+      # Firefox.app (app)
+      #
+      # We extract the version number (`48.0.1`) and compare it with the
+      # installed path, because the version number is part of it. If the path
+      # is not found, like in the case above, it means an old version was
+      # installed. When the CLI output of `brew cask` changes, this has to be
+      # updated, too.
+      version_string = info_output.lines.first.chomp.sub(/^.*:./, '')
+      %r{^/usr/local/.*/#{version_string} } =~ info_output
     end
   end
 end
